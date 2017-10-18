52e8de673e63698ceb5f095dc52bc0c056845f06
Priyanka Singh <psprianca25@gmail.com>
2017-10-19 05:07:14 +0530

    Use map to store UserAccessLevel

--- src/managerImpl/UserAccessLevelManagerImpl.java
+++ src/managerImpl/UserAccessLevelManagerImpl.java
@@ -2,16 +2,12 @@ package managerImpl;
 
 import enums.ActionType;
 import enums.ResourceName;
-import managers.ResourceAuthManager;
+import managers.ResourceAccessManager;
 import managers.UserAccessLevelManager;
-import pojo.ResourceAuthorization;
-import pojo.UserAccessLevel;
+import managers.UserManager;
+import pojo.ResourceAccess;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.*;
 
 /**
  * @author priyanka_s
@@ -19,149 +15,147 @@ import java.util.stream.Collectors;
  *         on 18/10/17.
  */
 public class UserAccessLevelManagerImpl implements UserAccessLevelManager{
-    private static List<UserAccessLevel> userAccessLevelList = new ArrayList<>();
+    private Map<Integer, Set<Integer>> userAccessLevelList = new HashMap<>();
 
-    ResourceAuthManager resourceAuthManager;
+    private ResourceAccessManager resourceAccessManager;
+    private UserManager userManager;
     private static final UserAccessLevelManagerImpl INSTANCE = new UserAccessLevelManagerImpl();
     public static UserAccessLevelManagerImpl getInstance() {
         return INSTANCE;
     }
 
     private UserAccessLevelManagerImpl() {
-        resourceAuthManager = ResourceAuthManagerImpl.getInstance();
+        resourceAccessManager = ResourceAccessManagerImpl.getInstance();
+        userManager = UserManagerImpl.getInstance();
     }
 
     @Override
-    public void assignAccess(int userId, int resourceAuthId) throws Exception {
-        validateArguments(userId, resourceAuthId);
+    public void assignAccess(int userId, int resourceAccessId) throws Exception {
+        validateArguments(userId, resourceAccessId);
 
-        List<UserAccessLevel> givenUserAccessLevelList = getUserAccessLevelList(userId);
-
-        /**
-         * RESOURCE ID WILL BE ADDED TO EMPTY RESOURCE_SET
-         * */
-        if(givenUserAccessLevelList.isEmpty()) {
-            Set<Integer> resourceAuthIdSet = new HashSet<>();
-            resourceAuthIdSet.add(resourceAuthId);
-            UserAccessLevel userAccessLevel = new UserAccessLevel(userId, resourceAuthIdSet);
-            userAccessLevelList.add(userAccessLevel);
-        } else {
-          /**
-           * RESOURCE ID WILL BE ADDED TO EXISTING RESOURCE_SET
-           * */
-            UserAccessLevel userAccessLevel = givenUserAccessLevelList.get(0);
-            Set<Integer> resourceAuthIdSet = userAccessLevel.getResourceAuthIdSet();
-            resourceAuthIdSet.add(resourceAuthId);
+        try {
+            if (userAccessLevelList.containsKey(userId)) {
+                /**
+                 * RESOURCE ID WILL BE ADDED TO EXISTING RESOURCE_SET
+                 * */
+                Set<Integer> resourceAccessIdSet = userAccessLevelList.get(userId);
+                resourceAccessIdSet.add(resourceAccessId);
+            } else {
+                /**
+                 * RESOURCE ID WILL BE ADDED TO EMPTY RESOURCE_SET
+                 * */
+                Set<Integer> resourceAccessIdSet = new HashSet<>();
+                resourceAccessIdSet.add(resourceAccessId);
+                userAccessLevelList.put(userId, resourceAccessIdSet);
+            }
+        } catch (ClassCastException | NullPointerException | UnsupportedOperationException | IllegalArgumentException e) {
+            System.out.println("Exception occur while assigning access to user: " + userId);
+            e.printStackTrace();
         }
     }
 
     @Override
-    public void revokeAccess(int userId, int resourceAuthId) throws Exception {
-        validateArguments(userId, resourceAuthId);
-        List<UserAccessLevel> givenUserAccessLevelList = getUserAccessLevelList(userId);
-        /**NO USER_ACCESS LEVEL ENTRY PRESENT IN LIST*/
-        if(givenUserAccessLevelList.isEmpty()) {
-            throw new Exception("No UserAccessLevel present in list for given userId: " + userId);
-        } else {
+    public void revokeAccess(int userId, int resourceAccessId) throws Exception {
+        validateArguments(userId, resourceAccessId);
+
+        if (userAccessLevelList.containsKey(userId)) {
             /**
              * REMOVE FROM LIST
              * */
-            UserAccessLevel userAccessLevel = givenUserAccessLevelList.get(0);
-            Set<Integer> resourceAuthIdSet = userAccessLevel.getResourceAuthIdSet();
-            resourceAuthIdSet.remove(resourceAuthId);
+            try {
+                Set<Integer> resourceAccessIdSet = userAccessLevelList.get(userId);
+                resourceAccessIdSet.remove(resourceAccessId);
+            } catch (ClassCastException | NullPointerException | UnsupportedOperationException e) {
+                System.out.println("Unable to revoke access from user user: " + userId);
+                e.printStackTrace();
+            }
+        } else {
+            /**NO USER_ACCESS LEVEL ENTRY PRESENT IN LIST*/
+            throw new NoSuchElementException("No UserAccessLevel present in list for given userId: " + userId);
         }
+
     }
 
     @Override
     public boolean canAccess(int userId, ActionType actionType, ResourceName resourceName) throws Exception {
         validateUserId(userId);
-        /**
-         * NO USER_ACCESS LEVEL ENTRY PRESENT IN LIST
-         */
-        List<UserAccessLevel> givenUserAccessLevelList = getUserAccessLevelList(userId);
 
-        if(givenUserAccessLevelList.isEmpty()) {
-            throw new Exception("No UserAccessLevel present in list for given userId: " + userId);
+        Set<Integer> userResourceAccessIdSet = userAccessLevelList.get(userId);
+
+        /**
+         * IF NO RESOURCE ACCESS IS ASSIGNED TO USER i.e , MAP IS ENTRY IN NOT PRESENT
+         * */
+        if (userResourceAccessIdSet == null || userResourceAccessIdSet.isEmpty())
+        {
+            return false;
         }
 
-        UserAccessLevel userAccessLevel = givenUserAccessLevelList.get(0);
-
-        List<ResourceAuthorization> resourceAuthorizationList = ResourceAuthManagerImpl.getResourceAuthorizationList();
+        List<ResourceAccess> totalResourceAccessList = resourceAccessManager.getResourceAccessList();
 
         /**
-         * FIND RESOURCE_AUTH FROM RESOURCE_AUTH_LIST WHERE
-         * - RESOURCE_AUTH SHOULD BE PRESENT IN USER_ACCESS_LEVEL LIST
-         * - GIVEN RESOURCE_NAME == RESOURCE_AUTH_LIST RESOURCE NAME
+         * FIND RESOURCE_ACCESS FROM RESOURCE_ACCESS_LIST WHERE
+         * - RESOURCE_ACCESS SHOULD BE PRESENT IN USER_ACCESS_LEVEL LIST
+         * - GIVEN RESOURCE_NAME == RESOURCE_ACCESS_LIST RESOURCE NAME
          * - ACTION_TYPE_LEVEL >= GIVEN ACTION_TYPE_LEVEL
          * Eg: User having WRITE access can also READ
          */
 
          try {
-             Set<Integer> resourceAuthIdSet = userAccessLevel.getResourceAuthIdSet();
-             if (!resourceAuthIdSet.isEmpty()) {
-                 for (ResourceAuthorization resourceAuth : resourceAuthorizationList) {
-                     if (resourceAuthIdSet.contains(resourceAuth.getId()) && (resourceAuth.getResourceName().equals(resourceName) || resourceAuth.getResourceName().equals(ResourceName.ALLOW_ALL))) {
-                         if (resourceAuth.getAccessLevel().getActionType().value() >= actionType.value()) {
-                             return true;
-                         }
+             for (ResourceAccess resourceAccess : totalResourceAccessList) {
+                 if (userResourceAccessIdSet.contains(resourceAccess.getId()) && (resourceAccess.getResourceName().equals(resourceName) || resourceAccess.getResourceName().equals(ResourceName.ALLOW_ALL))) {
+                     if (resourceAccess.getAccessLevel().getActionType().value() >= actionType.value()) {
+                         return true;
                      }
                  }
              }
-         } catch (Exception e) {
+         } catch (ClassCastException | NullPointerException e) {
              e.printStackTrace();
-             throw new Exception("Exception occurs while checking User Resource access");
+             System.out.println("Unable to check User Resource access for user: " + userId);
          }
 
         return false;
     }
 
-    public static List<UserAccessLevel> getUserAccessLevelList()
+    @Override
+    public Map<Integer, Set<Integer>> getUserAccessLevelList()
     {
         return userAccessLevelList;
     }
 
-    public static UserAccessLevel getUserAccessLevel(int userId)
-    {
-        return userAccessLevelList
-                .stream()
-                .filter(p -> p.getUserId() == userId)
-                .collect(Collectors.toList())
-                .get(0);
-
-    }
-
-    private static List<UserAccessLevel> getUserAccessLevelList(int userId)
-    {
-        return userAccessLevelList
-                .stream()
-                .filter(p -> p.getUserId() == userId)
-                .collect(Collectors.toList());
-    }
-
     /**
-     * CHECK WHETHER USER_ID AND RESOURCE_ID IS PRESENT IN USER AND RESOURCE_AUTH LIST OR NOT
+     * CHECK WHETHER USER_ID AND RESOURCE_ID IS PRESENT IN USER AND RESOURCE_ACCESS LIST OR NOT
      * */
-    private static void validateArguments(int userId, int resourceId) throws Exception {
+    private void validateArguments(int userId, int resourceId) throws Exception {
+        if (resourceId <= 0)
+        {
+            throw new Exception("Invalid resource id:" + userId);
+        }
+
         validateUserId(userId);
 
-        boolean resourceExists = ResourceAuthManagerImpl.getResourceAuthorizationList()
+        boolean resourceExists = resourceAccessManager.getResourceAccessList()
                 .stream()
                 .anyMatch(p -> p.getId() == resourceId);
 
         if (!resourceExists)
         {
-            throw new Exception("Resource/User not present for id:" + resourceId);
+            throw new NoSuchElementException("Resource not present for id:" + resourceId);
         }
     }
 
-    private static void validateUserId(int userId) throws Exception {
-        boolean userExists = UserManagerImpl.getUserList()
+    private void validateUserId(int userId) throws Exception {
+        if (userId <= 0)
+        {
+            throw new Exception("Invalid user id:" + userId);
+        }
+
+        boolean userExists = userManager.getUserList()
                 .stream()
                 .anyMatch(p -> p.getId() == userId);
 
         if (!userExists)
         {
-            throw new Exception("User not present for id:" + userId);
+            throw new NoSuchElementException("User not present for id:" + userId);
         }
     }
 }
